7580918bea865bb481ddcf66b42c755945a902e4
andrewreedy <reedyandrew11@gmail.com>
2020-02-29 12:43:42 -0500

    SQL seeds

---
+++ schema.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE bagel;
+
+-- USE bagel;
+
+
+--  CREATE TABLE situations(
+--  id Int( 11 ) AUTO_INCREMENT NOT NULL,
+--  situation VARCHAR(500) NOT NULL,
+--  choiceA int NOT NULL,
+--  choiceB int NOT NULL,
+
+--   PRIMARY KEY ( `id` ) 
+--   );
